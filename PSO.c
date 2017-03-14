416a8bed2857233893ce506bdc07f16bf725ffb3
Douglas <douglas.cavalcanti@ufu.br>
2017-03-14 09:45:49 -0300

    Submissão dos arquivos do projeto

---
+++ PSO.c
@@ -0,0 +1,329 @@
+#include <math.h>
+#include <time.h>
+#include "PSO.h"
+
+typedef struct particula Particula;
+typedef struct velocidade Velocidade;
+
+struct velocidade {
+    int **swaps, tamanho;
+};
+
+struct particula {
+    int *posicao, *pb;
+    Velocidade velo;
+    float fitnessAtual, fitnessPb;
+};
+
+//Procedimentos auxiliares
+void inicializaParticula(Particula *p, int numDimensoes);
+void liberaParticula(Particula *p, int numDimensoes);
+
+void inicializaVelocidade(Velocidade *v);
+void liberaVetorSwaps(Velocidade *v);
+void insereSwap(Velocidade *v, int *swap);
+
+void liberaVetor(int **vetor, int n);
+void copiaVetor(int *v1, int *v2, int n);
+
+void inicializaPopulacao(Particula **particula, int numParticulas, int **gb,
+float *fitnessGb, float **grafo, float c1, float c2, int numDimensoes);
+float calculaFitness(int *posicao, float **grafo, int numDimensoes);
+
+
+//Operadores
+void alteraPosicao(int *posicao, Velocidade velo);
+void multiplicaVelocidade(Velocidade *velo, int escalar);
+void subtraiPosicoes(int *p1, int *p2, int numDimensoes, Velocidade *velo);
+void somaVelocidades(Velocidade *velo1, Velocidade velo2);
+
+
+int* pso(float w, float c1, float c2,
+          int numParticulas, int maxItera,
+          int numDimensoes, float **grafo, float *fitness) {
+
+    srand((unsigned)time(NULL));
+
+    int i, j;
+    int *gb;
+    Particula *particula;
+    float r1, r2, fitnessGb = -1;
+    Velocidade veloCognitiva, veloSocial;
+
+    inicializaPopulacao(&particula, numParticulas, &gb, &fitnessGb, grafo, c1, c2, numDimensoes);
+
+    for(i = 0; i < maxItera; ++i)
+    {
+        for(j = 0; j < numParticulas; ++j)
+        {
+            r1 = ((float) (rand()%1000))/1000;
+            r2 = ((float) (rand()%1000))/1000;
+
+            //Atualiza posição e fitness
+            alteraPosicao(particula[j].posicao, particula[j].velo);
+            particula[j].fitnessAtual = calculaFitness(particula[j].posicao, grafo, numDimensoes);
+
+            //Atualiza pb se necessário
+            if (particula[j].fitnessAtual < particula[j].fitnessPb) {
+                copiaVetor(particula[j].pb, particula[j].posicao, numDimensoes);
+                particula[j].fitnessPb = particula[j].fitnessAtual;
+            }
+
+            //Atualiza gb se necessário
+            if (particula[j].fitnessPb < fitnessGb) {
+                copiaVetor(gb, particula[j].posicao, numDimensoes);
+                fitnessGb = particula[j].fitnessPb;
+            }
+
+            //Atualiza velocidade
+
+            multiplicaVelocidade(&(particula[j]).velo, w);
+
+            inicializaVelocidade(&veloCognitiva);
+            inicializaVelocidade(&veloSocial);
+
+            subtraiPosicoes(particula[j].pb, particula[j].posicao, numDimensoes, &veloCognitiva);
+            subtraiPosicoes(gb, particula[j].posicao, numDimensoes, &veloSocial);
+
+            multiplicaVelocidade(&veloCognitiva, c1*r1);
+            multiplicaVelocidade(&veloSocial, c2*r2);
+
+            somaVelocidades(&(particula[j].velo), veloCognitiva);
+            somaVelocidades(&(particula[j].velo), veloSocial);
+
+            liberaVetorSwaps(&veloCognitiva);
+            liberaVetorSwaps(&veloSocial);
+
+
+        }
+    }
+
+    for (i = 0; i < numParticulas; ++i) {
+        liberaParticula(&(particula[i]), numDimensoes);
+    }
+
+    *fitness = fitnessGb;
+    return gb;
+}
+
+void inicializaPopulacao(Particula **particula, int numParticulas,
+                         int **gb, float *fitnessGb, float **grafo,
+                         float c1, float c2, int numDimensoes) {
+
+    float r2;
+    int i, j, k;
+
+    *gb = (int*) malloc(sizeof(int) * numDimensoes);
+    *particula = (Particula*) malloc(numParticulas * sizeof(Particula));
+
+    //Inicializa posição e pb de cada partícula e define o gb
+    for(i = 0; i < numParticulas; ++i)
+    {
+        inicializaParticula( &( (*particula)[i] ), numDimensoes);
+
+        //Randomiza posição
+        for (j = 0; j < numDimensoes; ++j) {
+
+            int flag = 0;
+
+            while (!flag) {
+
+                (*particula)[i].posicao[j] = rand()%numDimensoes;
+                flag = 1;
+
+                for(k = 0; k < numDimensoes; ++k) {
+                    if((*particula)[i].posicao[j] == (*particula)[i].posicao[k] &&  j != k) {
+                        flag = 0;
+                    }
+                }
+            }
+        }
+
+        //Define fitness da posição atual e pb
+        (*particula)[i].fitnessAtual = calculaFitness((*particula)[i].posicao, grafo, numDimensoes);
+        (*particula)[i].pb = (int*) malloc(sizeof(int) * numDimensoes);
+        (*particula)[i].fitnessPb = (*particula)[i].fitnessAtual;
+        copiaVetor((*particula)[i].pb, (*particula)[i].posicao, numDimensoes);
+
+        //Checa se pb da particula é o gb
+        if ((*particula)[i].fitnessPb < *fitnessGb || *fitnessGb == -1) {
+            copiaVetor(*gb, (*particula)[i].posicao, numDimensoes);
+            *fitnessGb = (*particula)[i].fitnessPb;
+        }
+    }
+
+    //Inicializa velocidade de cada partícula
+    for(i = 0; i < numParticulas; ++i)
+    {
+        r2 = ((float) (rand()%1000))/1000;
+        inicializaVelocidade(&((*particula)[i].velo));
+        subtraiPosicoes(*gb, (*particula)[i].posicao, numDimensoes, &((*particula)[i].velo));
+        multiplicaVelocidade(&((*particula)[i].velo), c2*r2);
+
+    }
+}
+
+
+void inicializaParticula(Particula *p, int numDimensoes) {
+    p->pb = (int*) malloc(sizeof(int) * numDimensoes);
+    p->posicao = (int*) malloc(sizeof(int) * numDimensoes);
+    inicializaVelocidade(&(p->velo));
+}
+
+void liberaParticula(Particula *p, int numDimensoes) {
+    free(p->pb);
+    p->pb = NULL;
+
+    free(p->posicao);
+    liberaVetorSwaps(&(p->velo));
+    p->posicao = NULL;
+}
+
+void inicializaVelocidade(Velocidade *v) {
+    v->swaps = (int**) malloc(sizeof(int*));
+    v->swaps[0] = (int*) malloc(sizeof(int));
+    v->swaps[0][0] = -1;
+    v->swaps[0][1] = -1;
+    v->tamanho = 0;
+}
+
+void liberaVetorSwaps(Velocidade *v) {
+    int i;
+    for (i = 0; i < v->tamanho; ++i) {
+        free(v->swaps[i]);
+    }
+    free(v->swaps);
+    v->swaps = NULL;
+    v->tamanho = 0;
+}
+
+void insereSwap(Velocidade *v, int *swap) {
+    int i, **newSwaps, tamanhoVelocidade;
+
+    newSwaps = (int**) malloc(sizeof(int*) * (2 + v->tamanho));
+    for (i = 0; i < v->tamanho; ++i) {
+        newSwaps[i] = (int*) malloc(sizeof(int) * 2);
+        copiaVetor(newSwaps[i], v->swaps[i], 2);
+    }
+
+    tamanhoVelocidade = v->tamanho;
+    liberaVetorSwaps(v);
+
+    newSwaps[i] = (int*) malloc(sizeof(int) * 2);
+    newSwaps[i][0] = swap[0];
+    newSwaps[i][1] = swap[1];
+
+    newSwaps[i+1] = (int*) malloc(sizeof(int) * 2);
+    newSwaps[i+1][0] = -1;
+    newSwaps[i+1][1] = -1;
+
+    v->swaps = newSwaps;
+    v->tamanho = tamanhoVelocidade + 1;
+}
+
+void copiaVetor(int *v1, int *v2, int n) {
+    int i;
+    for(i = 0; i < n; ++i) {
+        v1[i] = v2[i];
+    }
+}
+
+float calculaFitness(int *posicao, float **grafo, int numDimensoes) {
+    int i;
+    float custo = 0;
+
+    for (i = 1; i < numDimensoes; ++i) {
+        custo += grafo[posicao[i-1]][posicao[i]];
+    }
+    return custo;
+}
+
+void alteraPosicao(int *posicao, Velocidade velo) {
+
+    int swap[2], i, aux;
+
+    for(i = 0; i < velo.tamanho; ++i) {
+        copiaVetor(swap, velo.swaps[i], 2);
+        aux = posicao[swap[0]];
+        posicao[swap[0]] = posicao[swap[1]];
+        posicao[swap[1]] = aux;
+    }
+}
+
+void multiplicaVelocidade(Velocidade *velo, int escalar) {
+
+    int i;
+    Velocidade *novaVelo, veloTruncada;
+
+    novaVelo = (Velocidade*) malloc(sizeof(Velocidade));
+
+    inicializaVelocidade(novaVelo);
+
+
+    for (i = 0; i < (int)escalar; ++i) {
+        somaVelocidades(novaVelo, *velo);
+    }
+
+    if (escalar - (int)escalar > 0) {
+        inicializaVelocidade(&veloTruncada);
+        for (i = 0; i < (int)(velo->tamanho/(escalar - (int)escalar)); ++i) {
+            insereSwap(&veloTruncada, velo->swaps[i]);
+        }
+        somaVelocidades(novaVelo, veloTruncada);
+    }
+
+    liberaVetorSwaps(velo);
+    velo->swaps = novaVelo->swaps;
+    velo->tamanho = novaVelo->tamanho;
+}
+
+void subtraiPosicoes(int *p1, int *p2, int numDimensoes, Velocidade *velo) {
+
+    int i, j, swap[2];
+
+
+    liberaVetorSwaps(velo);
+    inicializaVelocidade(velo);
+
+    for (i = 0; i < numDimensoes; ++i) {
+        for(j = 0; j < numDimensoes; ++j) {
+            if (p1[i] == p2[j] && i != j) {
+                swap[0] = i;
+                swap[1] = j;
+                insereSwap(velo, swap);
+            }
+        }
+    }
+}
+
+void somaVelocidades(Velocidade *velo1, Velocidade velo2) {
+    int i, tamanhoNovaVelo, **swaps;
+
+    tamanhoNovaVelo = velo1->tamanho + velo2.tamanho;
+    swaps = (int**) malloc(sizeof(int*) * (1 + tamanhoNovaVelo));
+
+    for (i = 0; i < velo1->tamanho; ++i) {
+        swaps[i] = (int*) malloc(sizeof(int) * 2);
+        copiaVetor(swaps[i], velo1->swaps[i], 2);
+    }
+    if (velo1->tamanho > 0) {
+        for (i = velo1->tamanho; i < tamanhoNovaVelo; ++i) {
+            swaps[i] = (int*) malloc(sizeof(int) * 2);
+            copiaVetor(swaps[i], velo2.swaps[i - velo1->tamanho], 2);
+        }
+    } else {
+        for (i = 0; i < velo2.tamanho; ++i) {
+            swaps[i] = (int*) malloc(sizeof(int) * 2);
+            copiaVetor(swaps[i], velo2.swaps[i], 2);
+        }
+    }
+
+    swaps[i] = (int*) malloc(sizeof(int) * 2);
+
+    swaps[i][0] = -1;
+    swaps[i][1] = -1;
+
+    liberaVetorSwaps(velo1);
+    velo1->swaps = swaps;
+    velo1->tamanho = tamanhoNovaVelo;
+}
---
+++ main.c
@@ -0,0 +1,148 @@
+#include "PSO.h"
+
+void carregaInstanciaTSP(float ***grafo, int *qtdCidades, char *nomeArquivo);
+int menu();
+
+int main()
+{
+    float w, c1, c2, **grafo, fitness;
+    int numParticulas, maxItera, numDimensoes, *resultado, i, numExecucoes, somatorioFitness, opcao;
+    char nomeArquivo[100];
+
+    resultado = NULL;
+    grafo = NULL;
+    opcao = -1;
+
+    w = 0.5;
+    c1 = 2.1;//cognitivo
+    c2 = 2.1;//social
+
+    printf("Informe o nome do arquivo de instancia (max 100): ");
+    scanf("%s", nomeArquivo);
+    printf("Informe o numero de particulas: ");
+    scanf("%d", &numParticulas);
+    printf("Informe o numero maximo de iteracoes: ");
+    scanf("%d", &maxItera);
+
+    system("cls");
+
+    carregaInstanciaTSP(&grafo, &numDimensoes, nomeArquivo);
+
+    while (opcao != 4 || opcao == -1) {
+        opcao = menu();
+        switch (opcao) {
+        case 0:
+            resultado = pso(w, c1, c2, numParticulas, maxItera, numDimensoes, grafo, &fitness);
+            printf("Fitness: %f\n\nRota:\n\n", fitness);
+            for (i = 0; i < numDimensoes; ++i)
+            {
+                printf("%d\n", resultado[i]);
+            }
+            free(resultado);
+            resultado = NULL;
+            system("pause");
+            system("cls");
+            break;
+        case 1:
+            printf("Informe o numero de execucoes: ");
+            scanf("%d", &numExecucoes);
+            somatorioFitness = 0;
+            for (i = 0; i < numExecucoes; ++i) {
+                pso(w, c1, c2, numParticulas, maxItera, numDimensoes, grafo, &fitness);
+                somatorioFitness += fitness;
+            }
+            printf("Media fitness: %d\n", somatorioFitness/numExecucoes);
+            system("pause");
+            system("cls");
+            break;
+        case 2:
+            for (i = 0; i < numDimensoes; ++i) {
+                free(grafo[i]);
+            }
+            free(grafo);
+            grafo = NULL;
+
+            printf("Informe o nome do novo arquivo de instancia (max 100): ");
+            scanf("%s", nomeArquivo);
+            carregaInstanciaTSP(&grafo, &numDimensoes, nomeArquivo);
+            system("cls");
+            break;
+        case 3:
+            printf("Numero de iteracoes atual: %d\n", maxItera);
+            printf("Numero de particulas atual: %d\n\n", numParticulas);
+            printf("Informe o numero maximo de iteracoes: ");
+            scanf("%d", &maxItera);
+            printf("Informe o numero de particulas: ");
+            scanf("%d", &numParticulas);
+            system("cls");
+            break;
+        default:
+            break;
+        }
+    }
+    if (grafo != NULL) {
+        for (i = 0; i < numDimensoes; ++i) {
+            free(grafo[i]);
+        }
+    }
+    free(grafo);
+
+    return 0;
+}
+
+
+void carregaInstanciaTSP(float ***grafo, int *qtdCidades, char *nomeArquivo)
+{
+    int i, j;
+    FILE *f;
+
+    f = fopen(nomeArquivo, "r");
+
+    if(f == NULL)
+    {
+        exit(1);
+    }
+
+    fscanf(f, "%d\n", qtdCidades);
+
+    *grafo = (float**) malloc((*qtdCidades) * sizeof(float*));
+    if(*grafo == NULL)
+    {
+        exit(1);
+    }
+
+    for(i = 0; i < *qtdCidades; ++i)
+    {
+        (*grafo)[i] = (float*) malloc((*qtdCidades) * sizeof(float));
+        if((*grafo)[i] == NULL)
+        {
+            exit(1);
+        }
+    }
+
+
+    for(i = 0; i < *qtdCidades - 1; ++i)
+    {
+        for(j = i + 1; j < *qtdCidades; ++j)
+        {
+            fscanf(f, "%f\n", &(*grafo)[i][j]);
+            (*grafo)[j][i] = (*grafo)[i][j];
+        }
+    }
+
+    fclose(f);
+}
+
+int menu() {
+    int opcao;
+    printf("Selecione uma das opcoes abaixo:\n");
+    printf(" 0 - Executar;\n");
+    printf(" 1 - Realizar execucao multipla;\n");
+    printf(" 2 - Alterar instancia tsp;\n");
+    printf(" 3 - Alterar nr. de iteracoes e nr. de particulas;\n");
+    printf(" 4 - Sair;\n");
+    printf("Opcao escolhida: ");
+    scanf("%d", &opcao);
+    system("cls");
+    return opcao;
+}
